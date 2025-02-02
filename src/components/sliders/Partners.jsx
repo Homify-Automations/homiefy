8703c6a210d42284e358a9b4a3cbf1b72e5b05df
Kapil Rajput <rajputkapil436@gmail.com>
2025-02-02 20:44:30 +0530

    almost done

--- src/components/sliders/Partners.jsx
+++ src/components/sliders/Partners.jsx
@@ -1,30 +1,37 @@
-import { SliderProps } from "@/src/common/sliderProps";
 import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
-import Data from '@data/sliders/partners';
+import Data from "@data/sliders/partners";
+
+const sliderSettings = {
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 80,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+};
 
 const PartnersSlider = () => {
   return (
-    <>
-    {/* partners */}
     <div className="mil-soft-bg">
-        <div className="container mil-p-0-120">
-            <Swiper
-                {...SliderProps.milInfiniteSlider}
-                className="swiper-container mil-infinite-show mil-up"
-            >
-                {Data.items.map((item, key) => (
-                <SwiperSlide className="swiper-slide" key={`partners-slider-item-${key}`}>
-                <a href={item.link} target="_blank" className="mil-partner-frame" style={{"width": "60px"}}>
-                    <img src={item.image} alt={item.alt} />
-                </a>
-                </SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
+      <div className="container mil-p-0-120">
+        <Swiper {...sliderSettings} className="swiper-container mil-infinite-show mil-up">
+          {Data.items.map((item, index) => (
+            <SwiperSlide key={index} style={{ width: "auto" }}>
+              <a href={item.link} target="_blank" className="mil-partner-frame" style={{ width: "80px" }}>
+                <img src={item.image} alt={item.alt} />
+              </a>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
-    {/* partners end */}
-    </>
   );
 };
+
 export default PartnersSlider;
